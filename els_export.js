46a8013acb6aee76e5dc9d2627881da53a5821ae
Cesar Macias <cesar.alejandro.macias@gmail.com>
2021-04-22 19:58:09 -0500

    dotobject

--- els_export.js
+++ els_export.js
@@ -8,10 +8,10 @@ const argv = require("minimist")(process.argv.slice(2));
     FUNCTION GET VALUE FROM OBJECT USING DOT STRING FROMAT
 */
 function dotObject(obj, is, value) {
-  if (typeof is == "string") return index(obj, is.split("."), value);
+  if (typeof is == "string") return dotObject(obj, is.split("."), value);
   else if (is.length == 1 && value !== undefined) return (obj[is[0]] = value);
   else if (is.length == 0) return obj;
-  else return index(obj[is[0]], is.slice(1), value);
+  else return dotObject(obj[is[0]], is.slice(1), value);
 }
 /*
     FUNCTION OBJECT TO EXPAND DOT ANNOTATION TO MULTI-LEVEL OBJECT

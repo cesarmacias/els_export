7708b57cd7cb8221e313bc52b7c65fb586657b9b
cesarmacias <cesar.alejandro.macias@gmail.com>
2023-12-06 16:09:25 -0500

    lodash

--- els_export.js
+++ els_export.js
@@ -2,10 +2,10 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 "use strict";
 
-const { Client } = require("es7");
+const { Client } = require("@elastic/elasticsearch");
 const fs = require("fs");
 const argv = require("minimist")(process.argv.slice(2));
-const merge = require("deepmerge");
+const _ = require("lodash");
 const func = require("./tools.js");
 
 /*
@@ -14,68 +14,69 @@ const func = require("./tools.js");
 async function DslQuery(config, objReplace, esClient, strDsl, timeFrom) {
   try {
     let dsl = strDsl;
-    if (func.isObject(objReplace)) {
+
+    if (typeof objReplace === "object" && objReplace !== null) {
       for (const key in objReplace) {
-        dsl =
-          typeof key == "string"
-            ? dsl.replace("%{" + key + "}", objReplace[key])
-            : dsl.replace('"%{' + key + '}"', objReplace[key]);
+        const replaceKey = `%{${key}}`;
+        dsl = dsl.split(replaceKey).join(objReplace[key]);
       }
     }
+
     const query = JSON.parse(dsl);
     const search = {
       index: config.query.index,
       body: query,
     };
-    let flag = true;
-    while (flag) {
+
+    while (true) {
       const { body } = await esClient.search(search);
       let array = [];
+
       if (config.query.type === "composite") {
-        if (
-          "aggregations" in body &&
-          func.isObject(body.aggregations) &&
-          config.query.aggs in body.aggregations
-        ) {
-          let obj = body.aggregations[config.query.aggs];
-          array = "buckets" in obj ? obj.buckets : [];
-          let after = "after_key" in obj ? obj.after_key : false;
-          if (func.isObject(after)) {
-            search.body["aggs"][config.query.aggs].composite.after = after;
+        const aggs = body.aggregations;
+        if (aggs && typeof aggs === "object" && config.query.aggs in aggs) {
+          const obj = aggs[config.query.aggs];
+          array = obj.buckets || [];
+          const after = obj.after_key || false;
+
+          if (typeof after === "object" && after !== null) {
+            search.body.aggs[config.query.aggs].composite.after = after;
           } else {
-            flag = false;
+            break;
           }
         } else {
-          flag = false;
+          break;
         }
       } else if (config.query.type === "search") {
-        if (
-          "hits" in body &&
-          "hits" in body.hits &&
-          body.hits.hits.length > 0
-        ) {
-          array = body.hits.hits;
-          let last = array[array.length - 1];
-          if ("sort" in last && Array.isArray(last.sort)) {
+        const hits = body.hits.hits;
+
+        if (hits.length > 0) {
+          array = hits;
+          const last = array[array.length - 1];
+
+          if (last.sort && Array.isArray(last.sort)) {
             search.body.search_after = last.sort;
           } else {
-            flag = false;
+            break;
           }
         } else {
-          flag = false;
+          break;
         }
       }
+
       for await (const item of array) {
-        if (func.isObject(item)) {
+        if (typeof item === "object" && item !== null) {
           let resp = {};
-          if ("_source" in item && "_index" && item) {
+          if ("_source" in item && "_index" in item) {
             resp = item["_source"];
           } else {
-            for (let key in item) {
-              if (key === "key" && func.isObject(item.key)) {
-                for (let field in item.key) {
-                  resp[field] = item.key[field];
-                }
+            for (const key in item) {
+              if (
+                key === "key" &&
+                typeof item.key === "object" &&
+                item.key !== null
+              ) {
+                Object.assign(resp, item.key);
               } else if (key === "doc_count") {
                 resp.count = item[key];
               } else if (key === config.query.exclude) {
@@ -84,15 +85,16 @@ async function DslQuery(config, objReplace, esClient, strDsl, timeFrom) {
                 resp[key] =
                   item[key].value != null ? item[key].value : undefined;
               } else if ("buckets" in item[key]) {
-                if (item[key]["buckets"].isArray) {
-                  let array = item[key]["buckets"];
-                  if (array.length == 1) {
-                    resp[key] = array[0]["doc_count"];
-                  } else if (array.length > 1) {
+                const bucketArray = item[key]["buckets"];
+                if (Array.isArray(bucketArray)) {
+                  if (bucketArray.length === 1) {
+                    resp[key] = bucketArray[0]["doc_count"];
+                  } else if (bucketArray.length > 1) {
                     resp[key] = {};
-                    for (let ob of array) {
+                    for (const ob of bucketArray) {
                       if (
-                        func.isObject(ob) &&
+                        typeof ob === "object" &&
+                        ob !== null &&
                         "doc_count" in ob &&
                         "key" in ob
                       ) {
@@ -102,12 +104,20 @@ async function DslQuery(config, objReplace, esClient, strDsl, timeFrom) {
                   }
                 } else {
                   resp[key] = {};
-                  let ob = item[key]["buckets"];
-                  for (let o in ob) {
-                    if (func.isObject(ob[o]) && "doc_count" in ob[o]) {
+                  const ob = item[key]["buckets"];
+                  for (const o in ob) {
+                    if (
+                      typeof ob[o] === "object" &&
+                      ob[o] !== null &&
+                      "doc_count" in ob[o]
+                    ) {
                       resp[key][o] = { count: ob[o].doc_count };
-                      for (let k in ob[o]) {
-                        if (func.isObject(ob[o][k]) && "value" in ob[o][k]) {
+                      for (const k in ob[o]) {
+                        if (
+                          typeof ob[o][k] === "object" &&
+                          ob[o][k] !== null &&
+                          "value" in ob[o][k]
+                        ) {
                           resp[key][o][k] = ob[o][k].value;
                         }
                       }
@@ -121,8 +131,12 @@ async function DslQuery(config, objReplace, esClient, strDsl, timeFrom) {
                 Object.keys(item[key]).length > 1
               ) {
                 resp[key] = {};
-                for (let k in item[key]) {
-                  if (func.isObject(item[key][k]) && "value" in item[key][k]) {
+                for (const k in item[key]) {
+                  if (
+                    typeof item[key][k] === "object" &&
+                    item[key][k] !== null &&
+                    "value" in item[key][k]
+                  ) {
                     resp[key][k] = item[key][k].value;
                   } else {
                     resp[key][k] = item[key][k];
@@ -135,6 +149,7 @@ async function DslQuery(config, objReplace, esClient, strDsl, timeFrom) {
               }
             }
           }
+
           if (
             "addTime" in config.export &&
             config.export.addTime &&
@@ -142,23 +157,22 @@ async function DslQuery(config, objReplace, esClient, strDsl, timeFrom) {
           ) {
             resp.time = timeFrom;
           }
-          let data = merge(
+
+          const data = _.merge(
             func.ObjExpand(resp),
             func.ObjExpand(config.export.attr) || {}
           );
+
           if (
             "format" in config.export &&
-            config.export.format == "csv" &&
+            config.export.format === "csv" &&
             "schema" in config.export &&
             config.export.schema.length > 0
           ) {
-            let delimiter = config.export.delimiter || ",";
-            let csv;
+            const delimiter = config.export.delimiter || ",";
+            let csv = "";
             config.export.schema.forEach((key, i) => {
-              csv =
-                i == 0
-                  ? func.dotObject(data, key)
-                  : csv + delimiter + func.dotObject(data, key);
+              csv += (i === 0 ? "" : delimiter) + func.dotObject(data, key);
             });
             console.log(csv);
           } else {
@@ -171,100 +185,102 @@ async function DslQuery(config, objReplace, esClient, strDsl, timeFrom) {
     if ("name" in e && "meta" in e) {
       console.error(e);
       console.error(JSON.stringify(e));
-    } else console.error(e);
+    } else {
+      console.error(e);
+    }
   }
 }
+
 /*
     FUNCTION TO RED CONFIG FILE - PREPARE THE LOOP FOR SEARCH
  */
 async function main(confFile, opt_delay) {
   try {
     const config = await func.ReadConfig(confFile);
-    const client = new Client({
-      nodes: config.elastic.nodes,
-      auth: {
-        username: config.elastic.username,
-        password: config.elastic.password,
-      },
-      maxRetries: config.elastic.maxRetries,
-      requestTimeout: config.elastic.requestTimeout,
-    });
-    if (fs.existsSync(config.query.file)) {
-      const strDsl = fs.readFileSync(config.query.file, "utf8");
-      let delay, timeFrom;
-      let maxIte = 1;
-      let part = 0;
-      let delta = 24 * 60 * 60;
-      let TimeRange = {};
-      let timeTo = 0;
-      if ("time" in config.query && func.isObject(config.query.time)) {
-        if ("lastDay" in config.query.time && config.query.time.lastDay) {
-          let dateFrom = ((d) => new Date(d.setDate(d.getDate() - 1)))(
-            new Date()
-          );
-          timeFrom =
-            new Date(
-              dateFrom.getFullYear(),
-              dateFrom.getMonth(),
-              dateFrom.getDate()
-            ).getTime() / 1000;
-          timeTo = timeFrom + delta;
-        } else if (
-          "dateFrom" in config.query.time &&
-          config.query.time.dateFrom.split("-").length == 3 &&
-          "toDays" in config.query.time &&
-          config.query.time.toDays > 0
-        ) {
-          maxIte = config.query.time.toDays;
-          let arrDate = config.query.time.dateFrom.split("-");
-          timeFrom =
-            new Date(+arrDate[0], +arrDate[1] - 1, +arrDate[2]).getTime() /
-            1000;
-          if (
-            "interval" in config.query.time &&
-            config.query.time.interval > 0
-          ) {
-            part = (24 * 60) / config.query.time.interval;
-            maxIte = maxIte * part;
-            delta = config.query.time.interval * 60;
-          }
-          timeTo = timeFrom + delta;
-        } else if (
-          "interval" in config.query.time &&
-          config.query.time.interval > 0
-        ) {
-          delay = opt_delay && opt_delay > 0 ? opt_delay * 60 : 0;
-          delay =
-            delay == 0 &&
-            "delay" in config.query.time &&
-            config.query.time.delay > 0
-              ? config.query.time.delay * 60
-              : delay;
-          timeTo = Math.round(Date.now() / 1000 - delay);
-          timeFrom = Math.round(timeTo - config.query.time.interval * 60);
-        } else {
-          throw new func.CustomError(
-            "ERROR_CONFIG",
-            "Not correct config for timeRange"
-          );
-        }
-        TimeRange = { _from: timeFrom, _to: timeTo };
-      }
-      for (let i = 0; i < maxIte; i++) {
-        const replace =
-          "vars" in config.query && func.isObject(config.query.vars)
-            ? { ...config.query.vars, ...TimeRange }
-            : TimeRange;
-        await DslQuery(config, replace, client, strDsl, timeFrom);
-        timeFrom = timeTo;
-        timeTo = timeFrom + delta;
-        TimeRange = { _from: timeFrom, _to: timeTo };
-      }
+
+    // Configuración de conexión a Elastic Cloud o Local
+    let clientOptions = {};
+    if (config.elastic.cloudId) {
+      clientOptions.cloud = {
+        id: config.elastic.cloudId,
+      };
     } else {
+      clientOptions.node = config.elastic.nodes;
+    }
+    clientOptions.auth = {
+      username: config.elastic.username,
+      password: config.elastic.password,
+    };
+    clientOptions.maxRetries = config.elastic.maxRetries;
+    clientOptions.requestTimeout = config.elastic.requestTimeout;
+
+    const client = new Client(clientOptions);
+
+    if (!fs.existsSync(config.query.file)) {
       throw new func.CustomError(
         "ERROR_CONFIG",
         "query DSL file not exists: " + config.query.file
       );
+    }
+
+    const strDsl = fs.readFileSync(config.query.file, "utf8");
+    let delay = 0;
+    let timeFrom = 0;
+    let timeTo = 0;
+    let delta = 24 * 60 * 60;
+    let maxIte = 1;
+    let part = 0;
+    let TimeRange = {};
+
+    if (config.query.time && config.query.time.constructor === Object) {
+      if (config.query.time.lastDay) {
+        const dateFrom = new Date();
+        dateFrom.setDate(dateFrom.getDate() - 1);
+        timeFrom = Math.floor(dateFrom.getTime() / 1000);
+        timeTo = timeFrom + delta;
+      } else if (
+        config.query.time.dateFrom &&
+        config.query.time.dateFrom.split("-").length === 3 &&
+        config.query.time.toDays > 0
+      ) {
+        maxIte = config.query.time.toDays;
+        const arrDate = config.query.time.dateFrom.split("-");
+        timeFrom = Math.floor(
+          new Date(+arrDate[0], +arrDate[1] - 1, +arrDate[2]).getTime() / 1000
+        );
+        if (config.query.time.interval > 0) {
+          part = (24 * 60) / config.query.time.interval;
+          maxIte *= part;
+          delta = config.query.time.interval * 60;
+        }
+        timeTo = timeFrom + delta;
+      } else if (config.query.time.interval > 0) {
+        delay = opt_delay && opt_delay > 0 ? opt_delay * 60 : 0;
+        delay =
+          delay === 0 && config.query.time.delay > 0
+            ? config.query.time.delay * 60
+            : delay;
+        timeTo = Math.round(Date.now() / 1000 - delay);
+        timeFrom = timeTo - config.query.time.interval * 60;
+      } else {
+        throw new func.CustomError(
+          "ERROR_CONFIG",
+          "Not correct config for timeRange"
+        );
+      }
+
+      TimeRange = { _from: timeFrom, _to: timeTo };
+    }
+
+    for (let i = 0; i < maxIte; i++) {
+      const replace =
+        config.query.vars && config.query.vars.constructor === Object
+          ? { ...config.query.vars, ...TimeRange }
+          : TimeRange;
+      await DslQuery(config, replace, client, strDsl, timeFrom);
+      timeFrom = timeTo;
+      timeTo = timeFrom + delta;
+      TimeRange = { _from: timeFrom, _to: timeTo };
     }
   } catch (error) {
     console.error(error.name, error.message);
@@ -272,6 +288,7 @@ async function main(confFile, opt_delay) {
     process.exit(0);
   }
 }
+
 /*
  STAR PROGRAM
  */
--- tools.js
+++ tools.js
@@ -1,99 +1,78 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
 "use strict";
 const fs = require("fs");
 
-/*
-    Custom Error Message
- */
 class CustomError extends Error {
 	constructor(ErrorName, ...params) {
-		// Pasa los argumentos restantes (incluidos los específicos del proveedor) al constructor padre
 		super(...params);
-		// Mantiene un seguimiento adecuado de la pila para el lugar donde se lanzó nuestro error (solo disponible en V8)
-		if (Error.captureStackTrace) {
-			Error.captureStackTrace(this, CustomError);
-		}
+		Error.captureStackTrace(this, CustomError);
 		this.name = ErrorName;
 	}
 }
-/*
-    FUNCTION GET OR SET VALUE FROM OBJECT USING DOT STRING FROMAT
-*/
-function dotObject(obj, is, value) {
-	if (typeof is == "string") return dotObject(obj, is.split("."), value); 
-	else if (is.length == 1 && value !== undefined) return (obj[is[0]] = value);
-	else if (is.length == 0) return obj;
-	else if (is[0] in obj) return dotObject(obj[is[0]], is.slice(1), value);
-	else return "";
-}
-/*
-    FUNCTION OBJECT TO EXPAND DOT ANNOTATION TO MULTI-LEVEL OBJECT
- */
-function parseDotNotation(str, val, obj) {
-	let currentObj = obj,
-		keys = str.split("."),
-		i,
-		l = Math.max(1, keys.length - 1),
-		key;
 
-	for (i = 0; i < l; ++i) {
-		key = keys[i];
+function dotObject(obj, is, value) {
+	if (typeof is === "string") {
+		return dotObject(obj, is.split("."), value);
+	} else if (is.length === 1 && value !== undefined) {
+		obj[is[0]] = value;
+		return obj;
+	} else if (is.length === 0) {
+		return obj;
+	} else if (is[0] in obj) {
+		return dotObject(obj[is[0]], is.slice(1), value);
+	} else {
+		return obj;
+	}
+}
+
+function parseDotNotation(str, val, obj) {
+	const keys = str.split(".");
+	let currentObj = obj;
+	for (let i = 0; i < keys.length - 1; i++) {
+		const key = keys[i];
 		currentObj[key] = currentObj[key] || {};
 		currentObj = currentObj[key];
 	}
-	currentObj[keys[i]] = val;
+	currentObj[keys[keys.length - 1]] = val;
 	delete obj[str];
 }
+
 function ObjExpand(obj) {
 	for (const key in obj) {
-		if (key.indexOf(".") !== -1) {
+		if (key.includes(".")) {
 			parseDotNotation(key, obj[key], obj);
 		}
 	}
 	return obj;
 }
-/*
-    FUNCTION VALIDATE IF VAR IS OBJECT
- */
-const isObject = function (a) {
-	return !!a && a.constructor === Object;
-};
-/*
-	FUNCTION READ CONFIG FILE AND PARSE ENV VARIABLES
-*/
-async function ReadConfig(file) {
-	let config;
-	if (fs.existsSync(file)) {
-	  try {
-		const strConf = fs.readFileSync(file, "utf8");
-		config = JSON.parse(strConf);
-	  } catch (e) {
-		//console.log(e);
-		throw new CustomError("ERROR_READ",e.message);
-	  }
-	  if ("env" in config && Array.isArray(config.env)) {
-		for (let str of config.env) {
-		  let val_env, value;
-		  val_env = dotObject(config, str);
-		  if (val_env) {
-			value = process.env[val_env];
-			if (value) dotObject(config, str, value);
-		  }
-		}
-	  }
-	  return config;
-	} else {
-	  throw new CustomError("ERROR_FILE","File not exists");
-	}
-  }
 
-/*
-Funciones a Exportar
- */
+async function ReadConfig(file) {
+	if (!fs.existsSync(file)) {
+		throw new CustomError("ERROR_FILE", "File not exists");
+	}
+
+	try {
+		const strConf = fs.readFileSync(file, "utf8");
+		const config = JSON.parse(strConf);
+
+		if ("env" in config && Array.isArray(config.env)) {
+			for (const str of config.env) {
+				const val_env = dotObject(config, str);
+				const value = process.env[val_env];
+				if (value) {
+					dotObject(config, str, value);
+				}
+			}
+		}
+
+		return config;
+	} catch (e) {
+		throw new CustomError("ERROR_READ", e.message);
+	}
+}
+
 module.exports = {
-	isObject,
 	ObjExpand,
 	dotObject,
 	CustomError,
-	ReadConfig
+	ReadConfig,
 };
